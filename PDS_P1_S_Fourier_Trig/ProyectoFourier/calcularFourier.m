fa9f38751d4ca06d511f8a5cf43a83481be4b8b7
diegonavarro95 <dnavarroa2000@alumno.ipn.mx>
2026-03-08 17:57:36 -0600

    Changes in project 01
    Minor changes in project 01 matlab logic

--- PDS_P1_S_Fourier_Trig/ProyectoFourier/calcularFourier.m
+++ PDS_P1_S_Fourier_Trig/ProyectoFourier/calcularFourier.m
@@ -1,20 +1,28 @@
 function [a0_tex, an_tex, bn_tex, w0_tex, serie_plano] = calcularFourier(func_str, lim_a_str, lim_b_str, N)
-    syms t n;
-    assume(n, 'integer');
-    
-    f = str2sym(func_str); 
-    lim_a_sym = str2sym(lim_a_str); 
+
+    syms t n
+    assume(n,'integer')
+
+    f = str2sym(func_str);
+    lim_a_sym = str2sym(lim_a_str);
     lim_b_sym = str2sym(lim_b_str);
-    
+
     T_sym = lim_b_sym - lim_a_sym;
-    w0_sym = 2*sym(pi)/T_sym;
+    w0_sym = 2*pi/T_sym;
 
+    % coeficientes
     a0 = simplify((2/T_sym) * int(f, t, lim_a_sym, lim_b_sym));
-    an = simplify((2/T_sym) * int(f * cos(n*w0_sym*t), t, lim_a_sym, lim_b_sym));
-    bn = simplify((2/T_sym) * int(f * sin(n*w0_sym*t), t, lim_a_sym, lim_b_sym));
+    an = simplify((2/T_sym) * int(f*cos(n*w0_sym*t), t, lim_a_sym, lim_b_sym));
+    bn = simplify((2/T_sym) * int(f*sin(n*w0_sym*t), t, lim_a_sym, lim_b_sym));
 
-    serie = a0/2 + symsum(an*cos(n*w0_sym*t) + bn*sin(n*w0_sym*t), n, 1, N);
+    % construir serie manualmente
+    serie = a0/2;
 
+    for k = 1:N
+        serie = serie + subs(an,n,k)*cos(k*w0_sym*t) + subs(bn,n,k)*sin(k*w0_sym*t);
+    end
+
+    % TEX para Java
     a0_tex = char(latex(a0));
     an_tex = char(latex(an));
     bn_tex = char(latex(bn));
@@ -22,14 +30,24 @@ function [a0_tex, an_tex, bn_tex, w0_tex, serie_plano] = calcularFourier(func_st
 
     serie_plano = char(serie);
 
-    % Graficación
+    % límites numéricos
     lim_a_num = double(lim_a_sym);
     lim_b_num = double(lim_b_sym);
 
-    figure(1); 
-    clf; 
-    fplot(f, [lim_a_num, lim_b_num], 'r--', 'LineWidth', 2); hold on;
-    fplot(serie, [lim_a_num, lim_b_num], 'b'); grid on;
-    title(['Aproximación N = ', num2str(N)]);
-    legend('Original', 'Serie de Fourier');
+    % gráfica
+    figure(1)
+    clf
+
+    fplot(f,[lim_a_num lim_b_num],'r--','LineWidth',2)
+    hold on
+
+    fplot(serie,[lim_a_num lim_b_num],'b','LineWidth',1.5)
+
+    grid on
+    title(['Aproximación Fourier  N = ', num2str(N)])
+    legend('Original','Serie de Fourier')
+
+    hold off
+    drawnow
+
 end
